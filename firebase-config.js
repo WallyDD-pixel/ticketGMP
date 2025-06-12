add99163cdff898e86f21b69e617fd0daf8ac8cd
WallyDD-pixel <wallydibombepro@gmail.com>
2025-06-12 05:22:21 +0200

    online

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA9TKKqbTO7KKMcp55clqaSU4toRjtePrI",
   authDomain: "trendy-c080e.firebaseapp.com",
   projectId: "trendy-c080e",
-  storageBucket: "trendy-c080e.appspot.com",
+  storageBucket: "gs://trendy-c080e",
   messagingSenderId: "448430543042",
   appId: "1:448430543042:web:45d24896b90a27d574ab5b",
   measurementId: "G-14B798P4VW"
